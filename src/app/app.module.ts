9a75b24bfa31554418b9282230ed49f785f225fc
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-09-12 12:46:34 -0300

    background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 // material angular io
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { LogInComponent } from '../app/pages/log-in/log-in.component';
-import { HomePageComponent } from '../app/pages/home-page/home-page.component';
-import { FormsComponent } from './forms/forms/forms.component';
+import { LogInComponent } from '../app/log-in/log-in.component';
+import { HomePageComponent } from '../app/home-page/home-page.component';
+import { FormsComponent } from './forms-auth/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
