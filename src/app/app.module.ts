4154289432a4776a6437cec92904393ec03cb6af
Kaybel <kblmrtnz@gmail.com>
2018-09-12 08:49:59 -0300

    act ahora si, en ng serve no actualiza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LogInComponent } from './log-in/log-in.component';
 
 // material angular io
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { LogInComponent } from '../app/pages/log-in/log-in.component';
+import { HomePageComponent } from '../app/pages/home-page/home-page.component';
+import { FormsComponent } from './forms/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
+    HomePageComponent,
+    FormsComponent,
   ],
   imports: [
     BrowserModule,
